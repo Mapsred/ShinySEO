42f080239dca559f62f2808667ab6bb1b9c670ae
maps_red <maps_red@yahoo.fr>
2016-12-06 20:10:41 +0100

    Add tabs to ui

--- inst/shinyseo/shinyseo/server.R
+++ inst/shinyseo/shinyseo/server.R
@@ -11,13 +11,13 @@ shinyServer(function(input, output, session) {
   keyword_list <- eventReactive(input$go, {input$keywords_input})
   domains <- c()
 
-  output$keywords_list <- renderTable(strsplit(x = keyword_list(), split = "\n")[[1]], striped = TRUE, hover = TRUE, bordered = TRUE)
-
   output$display <- renderPrint({
     if (keyword_list() != "") {
       wordList <- strsplit(x = keyword_list(), split = "\n")[[1]]
       result <- SEO::daily_run(string(), wordList, 30)
-      #print(result)
+
+      #output$keywords_list <- renderTable(wordList, striped = TRUE, hover = TRUE, bordered = TRUE, colnames = FALSE)
+      cat("Recherche journalière effectuée")
     }
 
   })
