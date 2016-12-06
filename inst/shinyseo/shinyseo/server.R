ce010cf9116879cd3b53909ca1037dfc80de7052
maps_red <maps_red@yahoo.fr>
2016-12-06 19:51:22 +0100

    Add daily result

--- inst/shinyseo/shinyseo/server.R
+++ inst/shinyseo/shinyseo/server.R
@@ -9,22 +9,18 @@ shinyServer(function(input, output, session) {
   string <- eventReactive(input$go,{input$titre})
   zoneText <- eventReactive(input$go, {input$zone})
   keyword_list <- eventReactive(input$go, {input$keywords_input})
-
-
-  output$display <- renderPrint({
-    cat(string())
-  })
-
-  output$zone <- renderPrint({
-    cat(zoneText())
-  })
-
+  domains <- c()
 
   output$keywords_list <- renderTable(strsplit(x = keyword_list(), split = "\n")[[1]], striped = TRUE, hover = TRUE, bordered = TRUE)
 
+  output$display <- renderPrint({
+    if (keyword_list() != "") {
+      wordList <- strsplit(x = keyword_list(), split = "\n")[[1]]
+      result <- SEO::daily_run(string(), wordList, 30)
+      #print(result)
+    }
 
-
-
+  })
 
 })
 
--- inst/shinyseo/shinyseo/ui.R
+++ inst/shinyseo/shinyseo/ui.R
@@ -7,10 +7,17 @@ library(shinyjs)
 library(rhandsontable)
 
 fluidPage(
+  mainPanel(
+    tabsetPanel(
+      tabPanel("Recherche"),
+      tabPanel("Summary"),
+      tabPanel("Table")
+    )
+  ),
+
   useShinyjs(), # on active shinyjs
   titlePanel("Data Analystic"),
   textInput(inputId = "titre", label = "Input Text"),
-  textAreaInput(inputId = "zone", label = "Text Area Input"),
   textAreaInput(inputId = "keywords_input", label = "Sur quel mot clef voulez-vous chercher le site ? Une expression par ligne"),
 
   textOutput("display"),
